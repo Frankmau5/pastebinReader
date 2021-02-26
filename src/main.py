a319af6272b0dcd49cc90b2144ce74365ca4462d
Knrf <mulvie@protonmail.com>
2021-02-26 14:58:41 +0000

    version 1.2.0

--- src/main.py
+++ src/main.py
@@ -20,6 +20,7 @@ class Application(Gtk.Application):
         GLib.set_application_name('Pastebin Reader')
         GLib.set_prgname('mlv.knrf.pastebinReader')
         self.backend  = App_Backend()
+        self.cat_name = "javascript"
         self.lang_mang = GtkSource.LanguageManager()
 
     def do_activate(self):
@@ -27,7 +28,7 @@ class Application(Gtk.Application):
         window.set_icon_name('mlv.knrf.pastebin_reader')
         window.set_titlebar(self.mk_title_bar())
         
-        window.add(self.mk_switch(self.mk_item_page(), self.mk_reader_page()))
+        window.add(self.mk_switch(self.mk_item_page(), self.mk_reader_page(), self.mk_category_page()))
         window.set_default_size(720, 1300)
         window.show_all()
     
@@ -115,8 +116,102 @@ class Application(Gtk.Application):
 
         scrolledwindow.add(source)
         return scrolledwindow
+       
+    def mk_category_page(self):
+        scrolledwindow = Gtk.ScrolledWindow()
+        scrolledwindow.set_hexpand(True)
+        scrolledwindow.set_vexpand(True)
         
-    def mk_switch(self,item_page,reader_page):
+        v_box = Gtk.VBox()
+
+        h_box_1 = Gtk.HBox()
+        h_box_1.set_homogeneous(True)
+        button1 = Gtk.RadioButton.new_with_label_from_widget(None, "javascript")
+        button2 = Gtk.RadioButton.new_with_label_from_widget(button1, "java")
+        h_box_1.add(button1)
+        h_box_1.add(button2)
+
+        h_box_2 = Gtk.HBox()
+        h_box_2.set_homogeneous(True)
+        button3 = Gtk.RadioButton.new_with_label_from_widget(button1, "python")
+        button4 = Gtk.RadioButton.new_with_label_from_widget(button1, "php")
+        h_box_2.add(button3)
+        h_box_2.add(button4)
+
+        h_box_3 = Gtk.HBox()
+        h_box_3.set_homogeneous(True)
+        button5 = Gtk.RadioButton.new_with_label_from_widget(button1, "c++ ")
+        button6 = Gtk.RadioButton.new_with_label_from_widget(button1, "c")
+        h_box_3.add(button5)
+        h_box_3.add(button6)
+
+        h_box_4 = Gtk.HBox()
+        h_box_4.set_homogeneous(True)
+        button7 = Gtk.RadioButton.new_with_label_from_widget(button1, "c#")
+        button8 = Gtk.RadioButton.new_with_label_from_widget(button1, "bash")
+        h_box_4.add(button7)
+        h_box_4.add(button8)
+
+        h_box_5 = Gtk.HBox()
+        h_box_5.set_homogeneous(True)
+        button9 = Gtk.RadioButton.new_with_label_from_widget(button1, "json")
+        button10 = Gtk.RadioButton.new_with_label_from_widget(button1, "sql")
+        h_box_5.add(button9)
+        h_box_5.add(button10)
+
+        h_box_6 = Gtk.HBox()
+        h_box_6.set_homogeneous(True)
+        button11 = Gtk.RadioButton.new_with_label_from_widget(button1, "html")
+        button12 = Gtk.RadioButton.new_with_label_from_widget(button1, "css")
+        h_box_6.add(button11)
+        h_box_6.add(button12)
+
+        h_box_7 = Gtk.HBox()
+        h_box_7.set_homogeneous(True)
+        button13 = Gtk.RadioButton.new_with_label_from_widget(button1, "perl")
+        button14 = Gtk.RadioButton.new_with_label_from_widget(button1, "powershell")
+        h_box_7.add(button13)
+        h_box_7.add(button14)
+
+        h_box_8 = Gtk.HBox()
+        h_box_8.set_homogeneous(True)
+        button15 = Gtk.RadioButton.new_with_label_from_widget(button1, "lua")
+        button16 = Gtk.RadioButton.new_with_label_from_widget(button1, "kotlin")
+        h_box_8.add(button15)
+        h_box_8.add(button16)
+
+        h_box_9 = Gtk.HBox()
+        h_box_9.set_homogeneous(True)
+        button17 = Gtk.RadioButton.new_with_label_from_widget(button1, "go")
+        button18 = Gtk.RadioButton.new_with_label_from_widget(button1, "rust")
+        h_box_9.add(button17)
+        h_box_9.add(button18)
+
+        h_box_10 = Gtk.HBox()
+        search_btn = Gtk.Button.new_with_label("Search")
+        search_btn.connect("clicked",self.search_btn_clicked) 
+        h_box_10.add(search_btn)
+
+        btns = [button1,button2,button3,button4,button5,button6,button7,button8,button9,button10,button11,button12,button13,button14,button15,button16,button17,button18]
+
+        for b in btns:
+            b.connect("toggled", self.on_button_toggled,b.get_label())
+
+        v_box.add(h_box_1)
+        v_box.add(h_box_2)
+        v_box.add(h_box_3)
+        v_box.add(h_box_4)
+        v_box.add(h_box_5)
+        v_box.add(h_box_6)
+        v_box.add(h_box_7)
+        v_box.add(h_box_8)
+        v_box.add(h_box_9)
+        v_box.add(h_box_10)
+
+        scrolledwindow.add(v_box)
+        return scrolledwindow
+
+    def mk_switch(self,item_page,reader_page, category_page):
         vbox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 5) 
 
         #Start of switch view
@@ -127,8 +222,9 @@ class Application(Gtk.Application):
         self.switch_stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT) 
         self.switch_stack.set_transition_duration(1000) 
 
-        self.switch_stack.add_titled(item_page, "tree", "Items")
-        self.switch_stack.add_titled(reader_page, "scrolledwindow", "Reader")
+        self.switch_stack.add_titled(item_page, "item", "Items")
+        self.switch_stack.add_titled(reader_page, "reader", "Reader")
+        self.switch_stack.add_titled(category_page, "category", "Category")
 
         self.switch_stack.show()
         self.switch_view.show()
@@ -154,8 +250,16 @@ class Application(Gtk.Application):
 
         return lang_name
 
-    
-    # when the user selects a row
+    def on_button_toggled(self, button, name):
+       self.cat_name = name
+
+    def search_btn_clicked(self,button):
+        url = "https://pastebin.com/archive/" + self.cat_name
+        data = self.backend.get_items(url)
+        if data != None:
+            self.tree.set_model(data)
+            self.switch_stack.set_visible_child(self.switch_stack.get_child_by_name("item"))
+
     def on_treeview_selection_changed(self, tview):
         url = ""
         tree_sel = tview.get_selection()
@@ -169,18 +273,17 @@ class Application(Gtk.Application):
         if text != None:
             self.textbuffer.set_text(text)
             syntax = self.fix_lang(syntax)
-            print(syntax) 
             lang = GtkSource.LanguageManager.get_language(self.lang_mang,str(syntax).lower())
             self.textbuffer.set_highlight_syntax(True)
             if lang != None:
                 self.textbuffer.set_language(lang)
             # switch page on switchstack
-            self.switch_stack.set_visible_child(self.switch_stack.get_child_by_name("scrolledwindow"))
+            self.switch_stack.set_visible_child(self.switch_stack.get_child_by_name("reader"))
 
     def on_about(self, model_button):
         #icon = GdkPixbuf.Pixbuf.new_from_file()
         about = Gtk.AboutDialog()
-        about.set_version("1.1.1")
+        about.set_version("1.2.0")
         #about.set_logo(icon)
         about.set_website("https://github.com/Frankmau5/pastebinReader")
         about.set_license("GPLv3 Read more here : https://github.com/Frankmau5/pastebinReader/blob/main/LICENSE")
@@ -219,9 +322,9 @@ class App_Backend:
         self.item_page = "https://pastebin.com/archive"
         self.root_url = "https://pastebin.com/"
         
-    def get_items(self):
+    def get_items(self, url="https://pastebin.com/archive"):
         try:
-            r = requests.get(self.item_page, headers=self.header)
+            r = requests.get(url, headers=self.header)
             store = Gtk.ListStore(str, str, str)
 
             if r.status_code == 200:
